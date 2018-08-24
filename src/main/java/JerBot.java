5294b7ae5425483f2da8f79ad27c8a927759d222
lujeremy <jeremy.lu@stern.nyu.edu>
2018-08-24 16:59:59 -0400

    Setting token and logging in

--- src/main/java/JerBot.java
+++ src/main/java/JerBot.java
@@ -1,2 +1,14 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
 public class JerBot {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "";
+        builder.setToken(token);
+
+        builder.build();
+    }
 }
