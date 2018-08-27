9cf8eff72330c869916b89b85935e88193bd4c6e
lujeremy <jeremy.lu@stern.nyu.edu>
2018-08-27 11:11:09 -0400

    Basic bot message interaction

--- src/main/java/JerBot.java
+++ src/main/java/JerBot.java
@@ -1,14 +1,29 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
-public class JerBot {
+public class JerBot extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         String token = "";
         builder.setToken(token);
+        builder.addEventListener(new JerBot());
 
         builder.build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println("We received a message from " +
+                event.getAuthor().getName() + ": " +
+                event.getMessage().getContentDisplay()
+        );
+
+        if (event.getMessage().getContentRaw().equals("!foo")) {
+            event.getChannel().sendMessage("bar").queue();
+        }
     }
 }
