dc4a706fb7383c926beece5bd026181838bc6215
lujer2b <jeremy.lu@stern.nyu.edu>
2018-10-11 14:48:30 -0400

    Worked on QueryCommand

--- src/main/java/io/jlu/jerbot/commands/QueryCommand.java
+++ src/main/java/io/jlu/jerbot/commands/QueryCommand.java
@@ -4,6 +4,8 @@ import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.jdbi.v3.core.Jdbi;
 
+import java.util.List;
+
 public class QueryCommand implements Command {
 
     final private Jdbi jdbi;
@@ -16,5 +18,12 @@ public class QueryCommand implements Command {
     public void handleEvent(MessageReceivedEvent event, String parameter) {
         MessageChannel channel = event.getChannel();
         String contentRaw = event.getMessage().getContentRaw();
+
+        List<String> workouts = this.jdbi.withHandle(handle ->
+                handle.createQuery("SELECT Workout FROM Workouts").mapTo(String.class).list());
+
+        for (int i = 0; i < workouts.size(); i++) {
+            channel.sendMessage("Recorded Workout: " + workouts.get(i)).queue();
+        }
     }
 }
