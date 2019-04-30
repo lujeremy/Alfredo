1b1021ccf9f403a4192b2078a8923e8fb47cff85
Jeremy Lu <Jeremy.Lu@stern.nyu.edu>
2019-04-30 16:13:01 -0400

    Removed redundant modifiers, pom update

--- src/main/java/io/jlu/jerbot/commands/Command.java
+++ src/main/java/io/jlu/jerbot/commands/Command.java
@@ -5,6 +5,5 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public interface Command {
 
 //    public void handleEvent(MessageReceivedEvent event);
-
-    public void handleEvent(MessageReceivedEvent event, String parameter);
+    void handleEvent(MessageReceivedEvent event, String parameter);
 }
