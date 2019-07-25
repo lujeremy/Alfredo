6590065322ad3b64387c1f3f5a83f45c0a2a2c48
lujeremy <jeremy.lu@stern.nyu.edu>
2019-07-25 01:11:32 -0400

    The creation of embeds!

--- src/main/java/io/jlu/alfredo/commands/ShowCommand.java
+++ src/main/java/io/jlu/alfredo/commands/ShowCommand.java
@@ -1,9 +1,12 @@
 package io.jlu.alfredo.commands;
 
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.jdbi.v3.core.Jdbi;
 
+import java.awt.*;
 import java.util.List;
 
 public class ShowCommand implements Command {
@@ -22,12 +25,20 @@ public class ShowCommand implements Command {
             List<String> workouts = this.jdbi.withHandle(handle ->
                     handle.createQuery("SELECT Workout FROM Workouts").mapTo(String.class).list());
 
-            StringBuffer workoutsMessage = new StringBuffer();
+            EmbedBuilder workoutsEmbed = new EmbedBuilder();
+            Member umaru = event.getGuild().getMemberById(482570873002262538L);
+
+            workoutsEmbed.setTitle("Recorded Workouts <:TohruPoint:507804401436459008>");
+            workoutsEmbed.setThumbnail(event.getAuthor().getAvatarUrl());
+            workoutsEmbed.setAuthor(umaru.getNickname(), "",umaru.getUser().getAvatarUrl());
+            workoutsEmbed.setColor(new Color(126, 249, 255));
+            if (workouts.size() == 0) workoutsEmbed.setTitle("Something went wrong");
+
             for (int i = 0; i < workouts.size(); i++) {
-                workoutsMessage.append("Recorded Workout: " + workouts.get(i) + "\n");
+              workoutsEmbed.addField("", workouts.get(i), false);
             }
 
-            channel.sendMessage(workoutsMessage).queue();
+            channel.sendMessage(workoutsEmbed.build()).queue();
         } catch (Exception e) {
             channel.sendMessage("Something went wrong but it isn't your fault!").queue();
             e.printStackTrace();
